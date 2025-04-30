df2453ddfa6f4e54204b951b1e938d683a42e6d6
Assar Rakhmetolla <asarrahmetollag@gmail.com>
2025-04-30 12:35:39 +0500

    few fixes

--- TildarMen/src/main/java/kz/tildarmen/TildarMen/mapper/GetEmployerProfileMapper.java
+++ TildarMen/src/main/java/kz/tildarmen/TildarMen/mapper/GetEmployerProfileMapper.java
@@ -1,18 +1,13 @@
 package kz.tildarmen.TildarMen.mapper;
 
 import kz.tildarmen.TildarMen.model.Employer;
-import kz.tildarmen.TildarMen.model.Location;
 import kz.tildarmen.TildarMen.requests.GetEmployerProfile;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GetEmployerProfileMapper {
 
+    @Mapping(source = "location.city", target = "location")
     GetEmployerProfile toDto(Employer employer);
-
-    default String map(Location location) {
-        if (location == null) return null;
-        return location.getCity();
-    }
-
 }
--- TildarMen/src/main/java/kz/tildarmen/TildarMen/mapper/TranslatorSettingsMapper.java
+++ TildarMen/src/main/java/kz/tildarmen/TildarMen/mapper/TranslatorSettingsMapper.java
@@ -1,7 +1,6 @@
 package kz.tildarmen.TildarMen.mapper;
 
 import kz.tildarmen.TildarMen.dto.TranslatorSettingsDto;
-import kz.tildarmen.TildarMen.model.Location;
 import kz.tildarmen.TildarMen.model.Translator;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,13 +8,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface TranslatorSettingsMapper {
 
-
-    @Mapping(target = "location", source = "location")
+    @Mapping(source = "location.city", target = "location")
     TranslatorSettingsDto toDto(Translator translator);
-
-    default String map(Location location) {
-        if (location == null) return null;
-        return location.getCity();
-    }
 
 }
