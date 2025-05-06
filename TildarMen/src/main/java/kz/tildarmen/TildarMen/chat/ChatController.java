c2bd5390b7098e4ce76964dd04f77cad84500b5f
Assar Rakhmetolla <asarrahmetollag@gmail.com>
2025-05-06 22:47:33 +0500

    message send

--- TildarMen/src/main/java/kz/tildarmen/TildarMen/chat/ChatController.java
+++ TildarMen/src/main/java/kz/tildarmen/TildarMen/chat/ChatController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,6 +36,12 @@ public class ChatController {
         notification.setRecipientId(savedMessage.getRecipientId());
         notification.setContent(savedMessage.getContent());
         messagingTemplate.convertAndSendToUser(savedMessage.getRecipientId(), "/queue/messages", notification);
+    }
+
+    @MessageMapping("/chat-sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage message){
+        return message;
     }
 
 }
